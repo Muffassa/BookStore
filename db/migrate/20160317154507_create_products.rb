b93dd1d9f8b195d5588d14b51942cbf4e56a5040
Nikita <keddofilatov@gmail.com>
2016-03-17 21:45:22 +0600

    Creat model product

---
+++ db/migrate/20160317154507_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.string :img_url
+
+      t.timestamps null: false
+    end
+  end
+end
