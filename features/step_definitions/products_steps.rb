322c1b9b2fb772463c032878d5c9afd159091c89
Nikita <keddofilatov@gmail.com>
2016-03-17 22:07:07 +0600

    Create route for products

--- features/step_definitions/products_steps.rb
+++ features/step_definitions/products_steps.rb
@@ -3,3 +3,7 @@ Given(/^I have books titled (.+)$/) do |titles|
     Product.create!(:title => title)
   end
 end
+
+When(/^I go to the list of products$/) do
+   visit products_path# Write code here that turns the phrase above into concrete actions
+end
