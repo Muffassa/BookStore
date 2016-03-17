a8d064a65155748db96fbb7d15bc52334e5eca74
Nikita <keddofilatov@gmail.com>
2016-03-17 14:23:25 +0600

    create static pages controller test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  describe "when get root path" do
+    render_views
+    it 'should render static_pages#index' do
+      get :index
+      expect(response.body).to match (/Книги/)
+    end
 
+  end
 end
