d0a0b151ce27b1fd056e2a2e0eb21a8ad44fc998
Nikita <keddofilatov@gmail.com>
2016-03-17 14:11:47 +0600

    Create static pages controller

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_path
+    it "should response root page" do
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
